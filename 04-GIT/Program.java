234f9c198ef09deabdeaecaedf36293ce750750f
ChihabEddine Zitouni <cezitouni@gmail.com>
2024-03-18 08:52:25 +0100

    update 2 program.java

--- 04-GIT/Program.java
+++ 04-GIT/Program.java
@@ -2,5 +2,6 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Sample Program for SE-2024 git lab");
         System.out.println("Edit 1");
+        System.out.println("Edit 2");
     }
 }
